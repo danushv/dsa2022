43b81ec6b043012c1bc66d58f11b0046b0555c14
danny boi <43886538+danushv@users.noreply.github.com>
2023-01-01 23:20:01 +0530

    Create code02.java

---
+++ Jan23/code02.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int sum(int num1, int num2) {
+        // Scanner input = new Scanner(System.in);
+		// int n= input.nextInt();
+		// int m= input.nextInt();
+        // int z= m+n;
+        // System.out.println(z);
+	
+ return num1 + num2;
+    
+}
+    
+}
+Console
