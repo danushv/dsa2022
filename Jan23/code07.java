c7cda82d184671daa7a9b78be8d1c24ca5c1bed4
danny boi <43886538+danushv@users.noreply.github.com>
2023-01-04 22:54:42 +0530

    Create code07.java

---
+++ Jan23/code07.java
@@ -0,0 +1,35 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input= new Scanner(System.in);
+		int n=input.nextInt();
+		for(int i=0;i<n;i++)
+		{
+		    
+		
+		int a =input.nextInt();
+		int b= input.nextInt();
+		if(a<b)
+		{
+		    System.out.println("<");
+		}
+		else if(a==b)
+		{
+		    System.out.println("=");
+		}
+		else
+		{
+		   System.out.println(">");
+		}
+		}
+	}
+}
